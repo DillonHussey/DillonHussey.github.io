4f870c646901ac2c785f239b6ae6f294b44338f1
Dillon Hussey <dillon.hussey@principia.edu>
2022-04-02 22:47:08 -0500

    working dynamic image loading

--- src/components/Project.js
+++ src/components/Project.js
@@ -13,21 +13,32 @@ prj {
 }
 
 */
-
-
-const Project = () => {
-
-  const prj = {
+/*
+const prj = {
     title: "Android App",
     description: "In this project i made an app about dogs that tells the user about employees dogs. It is written in Kotlin and cleanly uses layouts for nice presentation",
     form: "www.google.com"
   }
+*/
+
+
+
+const Project = ({prj}) => {
+
+  
 
   return (
       <div className='starter-template text-center mt-5'>
         <h1>{prj.title}</h1>
         <p className='lead text-capitalize'>{prj.description}</p>
         <p className='lead text-capitalize'>{prj.form}</p>
+        <img
+        alt="" 
+        src={prj.image.default}
+        width="300"
+        height="300"
+        className="d-flex align-items-center"
+        />
         
       </div>
   )
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,19 +1,49 @@
 import Header from '../components/Header'
 import Meta from '../components/Meta'
 import Project from '../components/Project'
+
+function importAll(dir) {
+  let images = {};
+  dir.keys().forEach((item) => { images[item.replace('./', '')] = dir(item)})
+  return images;
+}
+
+
+/*TODO:
+  - Put projects on a card view to be pretty :)
+  - Put Projects in JSON object so they can be loaded in
+  - Make project loader
+  - Set up file sharing
+  - Write about me sections and instructions for use.
+
+*/
+const images = importAll(require.context('../images', false, /\.(png|jpe?g|svg)$/));
+
 const Projects = () => {
+  console.log(images)
   // page content
   const pageTitle = "Projects"
   const pageDescription = 'Here I will show all the projects'
 
-  //test project to demo
+ 
+
+
+  const prj = {
+    title: "Android App",
+    description: "In this project i made an app about dogs that tells the user about employees dogs. It is written in Kotlin and cleanly uses layouts for nice presentation",
+    form: "www.google.com updated",
+    image : images['react.svg']
+  }
+
   
+
+
   return (
     <div>
       <Meta title={pageTitle}/>
-      <Project/>
+     
       <Header head={pageTitle} description={pageDescription}/>
-      
+      <Project prj={prj}/>
     </div>
   )
 }
