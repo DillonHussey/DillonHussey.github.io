2823f4263092d93dfa14390310b74cfeb0ba027e
Dillon Hussey <dillon.hussey@principia.edu>
2022-04-22 01:13:43 -0500

    starting to deploy

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
